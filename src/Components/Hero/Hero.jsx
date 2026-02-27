7ff239ded52eb6609df685623052599ed66a801e
Aniket Kumar <aniketpundir348@gmail.com>
2026-02-27 13:32:11 +0530

    Add slider in hero page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,37 @@
 import "./Hero.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import image1 from "../../assets/5.jpeg";
+import image2 from "../../assets/4.jpeg";
 
 export default function Hero() {
+
+    const images = [image1, image2];
+    const [current, setCurrent] = useState(0);
+
+    // Auto slide every 4 seconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const nextSlide = () => {
+        setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    };
+
+    const prevSlide = () => {
+        setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+    };
+
     return (
         <section className="hero">
 
             <div className="container hero__content">
                 <div className="hero__text">
-
                     <h1 className="hero__title">
                         Are you looking for caravan storage on Central Coast?
                     </h1>
@@ -18,14 +41,23 @@ export default function Hero() {
                             View Options
                         </Link>
                     </div>
-
                 </div>
             </div>
 
+            {/* Slider Background */}
             <div
                 className="hero__bg"
-                style={{ backgroundImage: `url(${image1})` }}
+                style={{ backgroundImage: `url(${images[current]})` }}
             />
+
+            {/* Arrows */}
+            <button className="hero__arrow hero__arrow--left" onClick={prevSlide}>
+                ❮
+            </button>
+
+            <button className="hero__arrow hero__arrow--right" onClick={nextSlide}>
+                ❯
+            </button>
 
         </section>
     );
