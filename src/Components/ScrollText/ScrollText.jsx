33c8021a081529c674ca6ce322fd8d56f6856aa7
Aniket Kumar <aniketpundir348@gmail.com>
2026-03-11 11:35:16 +0530

    Now all button is working

--- src/Components/ScrollText/ScrollText.jsx
+++ src/Components/ScrollText/ScrollText.jsx
@@ -13,7 +13,7 @@ const ScrollText = () => {
     return (
         <>
             <div className='scroll-text'>
-                <Link to="/book-online" onClick={() => { handleClick() }} className='scroll-quote .btn-primary'>
+                <Link to="/book-online" className='scroll-quote .btn-primary'>
                     Book online here
                 </Link>
                 <marquee className="scroll-text-content" >Safe • Secure • Conveniently Located • Affordable • 24/7 Access • Fully Fenced • Monitored Facility</marquee>
