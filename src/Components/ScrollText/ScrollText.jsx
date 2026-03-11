69e4e8fbbcc07294374de22779bddca1378d0d8b
Aniket Kumar <aniketpundir348@gmail.com>
2026-03-11 12:10:22 +0530

    Now all button is working

--- src/Components/ScrollText/ScrollText.jsx
+++ src/Components/ScrollText/ScrollText.jsx
@@ -4,24 +4,14 @@ import { Link } from 'react-router-dom'
 
 const ScrollText = () => {
 
-    const handleClick = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    };
+
     return (
         <>
             <div className='scroll-text'>
-                <Link to="/book-online" className='scroll-quote .btn-primary'>
+                <Link to="/book-online/book-online/book-online" className='scroll-quote .btn-primary'>
                     Book online here
                 </Link>
                 <marquee className="scroll-text-content" >Safe • Secure • Conveniently Located • Affordable • 24/7 Access • Fully Fenced • Monitored Facility</marquee>
-                {/* <div>
-                    <Link className='scroll-quote .btn-primary'>
-                        Request A quote
-                    </Link>
-                </div> */}
             </div>
         </>
     )
