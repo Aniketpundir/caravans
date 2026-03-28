aa794652d87f9dfe5b48d885b75500670632f936
Aniket Kumar <aniketpundir348@gmail.com>
2026-03-28 11:27:44 +0530

    Change Placeholder

--- src/Pages/MyBooking/MyBooking.jsx
+++ src/Pages/MyBooking/MyBooking.jsx
@@ -53,7 +53,7 @@ const MyBooking = () => {
                     <label>Username <span className="required">*</span></label>
                     <input
                         type="email"
-                        placeholder="Enter your email address"
+                        placeholder="Enter your username"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         disabled={loading}
